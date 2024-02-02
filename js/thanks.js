de6155b2bb8eb5a6e8e0d3853fe20a9133fc2493
Luis Mejia <hoyosmejia14o6@gmail.com>
2024-02-02 08:15:47 -0500

    add setTime

--- js/thanks.js
+++ js/thanks.js
@@ -5,3 +5,15 @@ const id = urlParams.get('id');
 function goBack() {
     window.location.href = `index.html?id=${id}`;
 }
+
+//función para regresar automáticamente después de 15 segundos
+function autoGoBack() {
+    setTimeout(() => {
+        goBack();
+    }, 2000); // 15 segundos en milisegundos
+}
+
+//Se llama la función para iniciar el temporizador cuando se carga la página
+
+    autoGoBack();
+
